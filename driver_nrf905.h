2e56115d9c8d12fa5a774932054c92b99933f5e2
Kyle Leong <kleong88@shaw.ca>
2025-08-19 15:19:40 -0700

    Finished with hex enumerations

--- driver_nrf905.h
+++ driver_nrf905.h
@@ -35,14 +35,74 @@ typedef enum
 } NRF905_pwr_db;
 
 
-//Config9[2:0] (en[2], freq[1:0])
+//Config1[4]
+typedef enum
+{
+    NRF905_RX_RED_PWR_EN = 0x10, 
+    NRF905_RX_RED_PWR_DISABLE = 0x00
+} NRF905_rx_reduced_pwr;
+
+
+//Config1[5]
+typedef enum
+{
+    NRF905_AUTO_RETRAN_EN = 0x20,
+    NRF905_AUTO_RETRAN_DISABLE = 0x00
+} NRF905_auto_retran;
+
+
+//Config2[2:0]
+typedef enum
+{
+    NRF905_RX_ADD_WIDTH_1 = 0x01, //1 byte address width
+    NRF905_RX_ADD_WIDTH_4 = 0x04  //4 byte address width
+} NRF905_rx_add_width;
+
+
+//Config2[6:4]
+typedef enum
+{
+    NRF905_TX_ADD_WIDTH_1 = 0x10, //1 byte address width
+    NRF905_TX_ADD_WIDTH_4 = 0x40  //4 byte address width
+} NRF905_tx_add_width;
+
+
+//Config3[5:0]
+typedef enum
+{
+    NRF905_RX_PAYLOAD_WIDTH_1  = 0x01, //1 byte
+    NRF905_RX_PAYLOAD_WIDTH_2  = 0x02, //2 bytes
+    NRF905_RX_PAYLOAD_WIDTH_32 = 0x20  //32 bytes
+} NRF905_rx_payload_width;
+
+
+//Config4[5:0]
+typedef enum
+{
+    NRF905_TX_PAYLOAD_WIDTH_1  = 0x01, //1 byte
+    NRF905_TX_PAYLOAD_WIDTH_2  = 0x02, //2 bytes
+    NRF905_TX_PAYLOAD_WIDTH_32 = 0x20  //32 bytes
+} NRF905_tx_payload_width;
+
+
+//Omitted RX address here, will be done manually in .c 
+
+
+//Config9[1:0]
+typedef enum
+{
+    NRF905_UP_CLK_4M   = 0x00, //4MHz
+    NRF905_UP_CLK_2M   = 0x01, //2MHz
+    NRF905_UP_CLK_1M   = 0x02, //1MHz
+    NRF905_UP_CLK_500K = 0x03 //500kHz
+} NRF905_up_clk_freq;
+
+
+//Config9[2]
 typedef enum
 {
     NRF905_UP_CLK_DISABLE = 0x00, //Disable output clock
-    NRF905_UP_CLK_4M   = 0x04, //4MHz
-    NRF905_UP_CLK_2M   = 0x05, //2MHz
-    NRF905_UP_CLK_1M   = 0x06, //1MHz
-    NRF905_UP_CLK_500K = 0x07 //500kHz
+    NRF905_UP_CLK_EN      = 0x04  //Enable output clock
 } NRF905_up_clk;
 
 
@@ -56,12 +116,20 @@ typedef enum
     NRF905_XOF_20 = 0x20  //20MHz
 } NRF905_xof;
 
+
 //Config9[7] 
 typedef enum
 {
     NRF905_CRC_MODE_8  = 0x00, //8 CRC check bit
     NRF905_CRC_MODE_16 = 0x80  //16 CRC check bit
 } NRF905_crc_mode;
+
+//Config9[6]
+typedef enum
+{
+    NRF905_CRC_EN = 0X40,      //Enable CRC mode
+    NRF905_CRC_DISABLE = 0x00  //Disable CRC mode
+} NRF905_crc;
 
 
 //Define functions to be written in .c driver
