8b17b8e17b8237df81bd66b43e96b957b5bffa82
Kyle Leong <kleong88@shaw.ca>
2025-09-03 13:43:47 -0700

    driver

--- driver_nrf905.c
+++ driver_nrf905.c
@@ -18,20 +18,6 @@ This .c file will implement the nrf905_t struct and all functions that were decl
 #define NRF905_R_RX_PAYLOAD        0x24
 #define NRF905_STATUS_REGISTER_NOP 0xFF
 
-/*
-void nrf905_write_config(nrf905_t* nrf, uint8_t start_reg, const uint8_t* data, uint8_t len); //Write n config bytes starting at a reg
-uint8_t nrf905_read_config(nrf905_t* nrf, uint8_t config_register); //Read back config bytes from a config register
-void nrf905_write_tx_payload(nrf905_t* nrf, const void* buf, uint8_t len); //Use a pointer to a buffer because the tx payload can be up to 32 bytes
-void nrf905_read_tx_payload(nrf905_t* nrf, void* buf, uint8_t len); //takes in a pointer of where to store the data in main.c
-void nrf905_write_tx_address(nrf905_t* nrf, const uint8_t* addr, uint8_t len); 
-void nrf905_read_tx_address(nrf905_t* nrf, void* buf, uint8_t len);
-void nrf905_read_rx_payload(nrf905_t* nrf, void* buf, uint8_t len);
-uint8_t nrf905_read_status(nrf905_t* nrf); //Send a NOP to receive status register contents (AM, DR, pg.25)
-void nrf905_spi_programming(nrf905_t* nrf); //Put RFIC in power down or standby mode to allow SPI programming (pg. 16)
-void nrf905_tx_mode(nrf905_t* nrf); //Put chip into TX mode
-void nrf905_rx_mode(nrf905_t* nrf); //Put chip into RX mode
-*/
-
 void nrf905_write_config(nrf905_t* nrf, uint8_t start_reg, const uint8_t* data, uint8_t len) {
     //Set instruction format 
     uint8_t opcode = NRF905_W_CONFIG | start_reg;
@@ -74,6 +60,7 @@ void nrf905_write_tx_payload(nrf905_t* nrf, const void* buf, uint8_t len) {
     nrf->ncs_high();
 }
 
+//Utilize HAL_SPI_TransmitReceive_DMA later (faster + frees cpu)
 void nrf905_read_tx_payload(nrf905_t* nrf, void* buf, uint8_t len) {
     uint8_t* out = (uint8_t*)buf;
     //Set instruction format
@@ -91,12 +78,66 @@ void nrf905_read_tx_payload(nrf905_t* nrf, void* buf, uint8_t len) {
 }
 
 
+void nrf905_write_tx_address(nrf905_t* nrf, const uint8_t* addr, uint8_t len) {
+    //Set instruction format
+    uint8_t opcode = NRF905_W_TX_ADDRESS;
+    //Pull ncs low
+    nrf->ncs_low();
+    //Send opcode
+    nrf->spi_xfer(opcode);
+    //Write len bytes 
+    nrf->spi_write(addr, len);
+    //Pull ncs high
+    nrf->ncs_high();
+}
 
 
-void nrf905_write_tx_address(nrf905_t* nrf, const uint8_t* addr, uint8_t len); 
-void nrf905_read_tx_address(nrf905_t* nrf, void* buf, uint8_t len);
-void nrf905_read_rx_payload(nrf905_t* nrf, void* buf, uint8_t len);
-uint8_t nrf905_read_status(nrf905_t* nrf); //Send a NOP to receive status register contents (AM, DR, pg.25)
+void nrf905_read_tx_address(nrf905_t* nrf, void* buf, uint8_t len) {
+    uint8_t* out = (uint8_t*)buf;
+    //Set instruction format
+    uint8_t opcode = NRF905_R_TX_ADDRESS;
+    //Pull ncs low
+    nrf-> ncs_low();
+    //Send opcode
+    nrf->spi_xfer(opcode);
+    //Send garbage data and store the address len times
+    for(int i = 0; i < len; i++) {
+        out[i] = nrf->spi_xfer(0x00);
+    }
+    //Pull ncs high
+    nrf->ncs_high();
+}
+
+//Utilize HAL_SPI_TransmitReceive_DMA later (faster + frees cpu)
+void nrf905_read_rx_payload(nrf905_t* nrf, void* buf, uint8_t len) {
+    uint8_t* out = (uint8_t*) buf;
+    //Set insturction format
+    uint8_t opcode = NRF905_R_RX_PAYLOAD;
+    //Pull ncs low
+    nrf-> ncs_low();
+    //Send opcode 
+    nrf->spi_xfer(opcode);
+    //Loop len times storing the data
+    for(uint8_t i = 0; i < len; i++) {
+        out[i] = nrf->spi_xfer(0x00);
+    }
+    //Pull ncs high
+    nrf->ncs_high();
+}
+
+//Send a NOP to receive status register contents (AM, DR, pg.25)
+uint8_t nrf905_read_status(nrf905_t* nrf) {
+    uint8_t out;
+    //Pull ncs low
+    nrf->ncs_low();
+    //Read the content coming from MISO
+    out = nrf->spi_xfer(0x00);
+    //Pull ncs high
+    nrf->ncs_high();
+    //Return the whole byte
+    return out;
+}
+
 
 void nrf905_spi_programming(nrf905_t* nrf) {
     //PWR_UP high, TRX_CE high
